cab1f15adae0aedc601bbe8310c6fdaf7bbf7b58
Mobashshir <83943245+Mobasheera@users.noreply.github.com>
2025-06-16 04:12:35 +0530

    Add files via upload

---
+++ PowerPlanToggle/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace PowerPlanToggle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.startWithWindowsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Text = "Power Plan Toggle";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startWithWindowsToolStripMenuItem,
+            this.toolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(207, 52);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            this.contextMenuStrip1.Click += new System.EventHandler(this.contextMenuStrip1_Click);
+            // 
+            // startWithWindowsToolStripMenuItem
+            // 
+            this.startWithWindowsToolStripMenuItem.CheckOnClick = true;
+            this.startWithWindowsToolStripMenuItem.Name = "startWithWindowsToolStripMenuItem";
+            this.startWithWindowsToolStripMenuItem.Size = new System.Drawing.Size(206, 24);
+            this.startWithWindowsToolStripMenuItem.Text = "Start with Windows";
+            this.startWithWindowsToolStripMenuItem.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(206, 24);
+            this.toolStripMenuItem1.Text = "Exit";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Opacity = 0D;
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem startWithWindowsToolStripMenuItem;
+    }
+}
+
---
+++ PowerPlanToggle/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Microsoft.Win32;
+
+namespace PowerPlanToggle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            notifyIcon1.Icon = new Icon(@"C:\Projects\PowerPlanToggle\GreenIcon.ico"); // College mode icon at startup
+            notifyIcon1.Visible = true; // This shows the icon in the system tray
+            notifyIcon1.Text = "College Plan";
+            this.Hide();
+            SetStartup();
+            CheckStartup();
+
+        }
+        // Replace these GUIDs with your real power plan GUIDs later
+        private string collegePlanGUID = "2dcf1035-f98a-4788-a7cf-645d2b5665db";
+        private string ultimatePlanGUID = "14ff874d-88ca-40d3-b80d-82e3c9f8ccfc";
+        private bool isCollegeMode = true;
+
+        private void SetStartup()
+        {
+            string appName = "PowerPlanToggle";
+            string exePath = Application.ExecutablePath;
+
+            RegistryKey reg = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+            if (reg.GetValue(appName) == null)
+            {
+                reg.SetValue(appName, exePath);
+            }
+        }
+        private void CheckStartup()
+        {
+            string appName = "PowerPlanToggle";
+            RegistryKey reg = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+
+            if (reg.GetValue(appName) != null)
+            {
+                startWithWindowsToolStripMenuItem.Checked = true;
+            }
+            else
+            {
+                startWithWindowsToolStripMenuItem.Checked = false;
+            }
+        }
+
+
+
+        //del
+
+        private void contextMenuStrip1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            string appName = "PowerPlanToggle";
+            string exePath = Application.ExecutablePath;
+
+            RegistryKey reg = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+
+            if (startWithWindowsToolStripMenuItem.Checked)
+            {
+                reg.SetValue(appName, exePath);
+            }
+            else
+            {
+                reg.DeleteValue(appName, false);
+            }
+
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                // Only toggle when left-clicked
+                if (isCollegeMode)
+                {
+                    Process.Start("powercfg", "/setactive " + ultimatePlanGUID);
+                    notifyIcon1.Text = "Ultimate Performance";
+                    notifyIcon1.Icon = new Icon(@"C:\Projects\PowerPlanToggle\RedIcon.ico");
+                    isCollegeMode = false;
+                }
+                else
+                {
+                    Process.Start("powercfg", "/setactive " + collegePlanGUID);
+                    notifyIcon1.Text = "College Plan";
+                    notifyIcon1.Icon = new Icon(@"C:\Projects\PowerPlanToggle\GreenIcon.ico");
+                    isCollegeMode = true;
+                }
+            }
+            // If right-click, do nothing (the context menu will open automatically)
+        }
+
+    }
+}
